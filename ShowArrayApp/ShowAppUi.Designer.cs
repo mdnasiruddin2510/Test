710fbb4fa4c771ec8bd789e824829d11297ce8a7
mdnasiruddin2510 <uddinnasir2510@gmail.com>
2019-02-08 10:09:30 +0600

    Hi

---
+++ ShowArrayApp/ShowAppUi.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ShowArrayApp
+{
+    partial class showApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(137, 44);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NameTextBox.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(51, 50);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name";
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(137, 90);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 2;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // showApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.NameTextBox);
+            this.Name = "showApp";
+            this.Text = "Show";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button ShowButton;
+    }
+}
+
---
+++ ShowArrayApp/ShowAppUi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowArrayApp
+{
+    public partial class showApp : Form
+    {
+        public showApp()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+        string[] names = new string[5];
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            names[i] = Convert.ToString(NameTextBox.Text);
+            MessageBox.Show(names[i]);
+            i++;
+        }
+    }
+}
